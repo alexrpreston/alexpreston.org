280d422eebdf179ed9aa217920e82142ce94782d
Alex Preston <apreston@smu.edu>
2020-10-01 13:05:07 -0500

    Added SMU to job title on resume

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls import url
 urlpatterns = [
     path('', include('portfolio.urls')),
     path('admin/', admin.site.urls),
+    url(r'^markdownx/', include('markdownx.urls')),
+    path('markdownx/', include('markdownx.urls')),
 ]
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdownify
+
 # Create your models here.
 
 STATUS = (
@@ -12,10 +15,12 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now= True)
-    content = models.TextField()
+    content = MarkdownxField()
     created_on = models.CharField(max_length=200, unique=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
+    def formatted_markdown(self):
+            return markdownify(self.body)
     class Meta:
         ordering = ['-created_on']
 
